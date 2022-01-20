166be23b7ba1733ae2f693c2fe204eb5a061554a
Harerimana Egide <egideharerimana085@gmail.com>
2022-01-20 14:33:38 +0200

    The first test

--- tests/services/index.test.js
+++ tests/services/index.test.js
@@ -1,30 +1,31 @@
 
-const {MongoClient} = require('mongodb');
-const {User} = require('../../mongodb/model')
+const { User } = require('../../mongodb/model')
 const mongoose = require('mongoose');
 
 let { checkEmail, createUser, editTodo, createTodo, deleteTodo, deleteUser } = require('../../mongodb/services')
 
 
 describe('create user', () => {
-  let db;
-
   beforeAll(async () => {
-    connection = await mongoose.connect('mongodb://127.0.0.1:27017/test', {
-      useNewUrlParser: true, 
+    await mongoose.connect('mongodb://127.0.0.1:27017/test', {
+      useNewUrlParser: true,
       useUnifiedTopology: true
-    });
-    db = mongoose.connection;
-  });
-  afterAll(async () => {
-    await db.close();
+    })
+      .then(instane => { console.log("[INFO] CONNECTED TO DB") })
+      .catch(e => { console.log("[ERROR] FAILED TO CONNECT TO DB") });
   });
 
-  
-    it('should insert a doc into collection', async () => {
-      const mockUser = {name: 'John Doe', email: 'doe@gmail.com',pass:'john'};
-      await createUser(mockUser.name,mockUser.email,mockUser.pass);
-      const insertedUser = await User.findOne({email: 'doe@gmail.com'});
-      expect(insertedUser.name).toEqual(mockUser.name);
-    });
+  afterAll(async () => {
+    for (const connection of mongoose.connections) {
+      await connection.close(true);
+    }
   });
+
+
+  it('should insert a doc into collection', async () => {
+    const mockUser = { name: 'John Doe', email: 'doe@gmail.com', pass: 'john' };
+    await createUser(mockUser.name, mockUser.email, mockUser.pass);
+    const insertedUser = await User.findOne({ email: 'doe@gmail.com' });
+    expect(insertedUser.name).toEqual(mockUser.name);
+  });
+});
--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,5 +1,5 @@
 var sum = require('./sum')
 
-test('adds 1 + 2 to equal 3', function() {
+it('adds 1 + 2 to equal 3', function() {
     expect(sum(1, 2)).toBe(3)
 })
