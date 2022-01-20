bd97194388258bc6625955a9c491b1d5e6f35377
Ange-Constance <constance.nimuhire@gmail.com>
2022-01-20 15:41:54 +0200

    'tests'

--- tests/integration/user.test.js
+++ tests/integration/user.test.js
@@ -10,4 +10,28 @@ describe('USER API INTEGRATION TESTS', () => {
             .expect(200);
     });
 
+    it("PUT /users/register",()=>{
+        supertest(app).put("users/register")
+        .send({name:"Kalisa", email:"kal@gmail.com", password:"12345677", confirmPassword:"12345677"})
+        .set('Accept', 'applcation/json')
+        .expect('Content-Type', /text/)
+        .expect(200);
+    }); 
+
+    it("GET /register",()=>{
+        supertest(app)
+        .get('/users')
+        .set('Accept', 'applcation/json')
+        .expect('Content-Type', /text/)
+        .expect(200);
+    }); 
+
+    it("DELETE /delete",()=>{
+        supertest(app)
+        .delete('/users')
+        .set('Accept', 'application/json')
+        .expect('Content-Type',/text/)
+        .expect(200);
+    })
+    
 });
