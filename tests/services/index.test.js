845e78eb74aa104d2bc3c7e9d82172bd4bfb1617
ishgervais <ishimwegervais11@gmail.com>
2022-01-20 11:23:58 +0200

    feat: create todo

--- tests/services/index.test.js
+++ tests/services/index.test.js
@@ -34,6 +34,25 @@ describe('create user', () => {
         expect(response).toEqual(mockedEmail)
     })
 
+
+    // to fix later
+    // it('delete user ',async()=>{
+    //   const mockedId = 'a61e8f4a9df624e0b4ab246c8';
+    //     const response = await deleteUser(mockedId)
+    //     console.log('yooo', response)
+    //     expect(response).toEqual(mockedId)
+    // })
+
+    it('should create todo', async () => {
+      const mockTODO = {id: '61e8f8f904023e223bc96a24', title: 'Wash shoes',dates:'20/1 10:59:00'};
+      await createTodo(mockTODO.id,mockTODO.title,mockTODO.dates);
+      const {todo} = await User.findOne({_id: mockTODO.id});
+      const  lastToDO = (todo[todo.length-1])
+      expect(lastToDO.title).toEqual(mockTODO.title);
+    });
+
+
+
     
 
 
